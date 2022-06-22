2b524775cc041d738e245de68ae54f8955df03cd
tgm4x <tgm4x@proton.me>
2022-06-22 08:21:39 +0800

    Add xmonad

--- install.sh
+++ install.sh
@@ -4,4 +4,6 @@
 # git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 stow -t ~/.config -S nvim
+stow -t ~/.xmonad -S xmonad
 stow -t ~ -S xorg
+stow -t ~ -S bash
